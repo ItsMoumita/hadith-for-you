bf57edea35be2c4d54f326071838f05447a0817f
Fatema-Tuj-Zahura Moumita <fatihamoumita98@gmail.com>
2026-06-08 00:56:25 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// Title : Basic Node app example
+// Descriptiom: Simple node application that print random hadith per second interval 
+// Author: Moumita
+
+
+
+
+
+
+
+
+
+
+
+console.log("Node.js is working")
