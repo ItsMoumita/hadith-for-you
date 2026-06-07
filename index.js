c75ed30d0cd2e488e24db7e16212e6aecf444683
Fatema-Tuj-Zahura Moumita <fatihamoumita98@gmail.com>
2026-06-08 01:54:44 +0600

    get random numbers between 2 numbers in javascript

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 // Descriptiom: Simple node application that print random hadith per second interval 
 // Author: Moumita
 
+// dependencies
+import math from "./lib/math.js"
 
 
-
-
+console.log(math.getRandomNumber(100,1003434));
 
 
 
